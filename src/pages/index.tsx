05a813e8cd4c8fe7613882462fef8bef1ef4e864
juan <juan.selvan77@gmail.com>
2023-07-16 17:48:50 +0800

    setup YT API + formatting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,25 @@
-import { Inter } from 'next/font/google'
-import {useState} from 'react'
+import { Inter } from "next/font/google";
+import { useState } from "react";
+import {useRouter} from "next/router";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [vid, setVid] = useState('')
+  const router = useRouter();
+  const [vid, setVid] = useState("");
 
   const handleSubmit = (e: { preventDefault: () => void }) => {
-    e.preventDefault()
-    console.log(vid)
-    const data = fetch('/api/getVideo', {
-      
-    }).then((res) => res.json())
-  }
+    e.preventDefault();
+    fetch(`/api/getVideo?link=${vid}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.response === "Video not found") {
+          console.log("f" + data.response);
+        } else {
+          console.log(data.response)
+        }
+      });
+  };
 
   return (
     <main>
@@ -21,16 +28,28 @@ export default function Home() {
         <h1>Dark Mode</h1>
       </div>
       <div className="flex-col w-3/5 mx-auto">
-        <h1 className={`text-center text-9xl font-semibold my-12 w-full`}>Enter your video link</h1>
+        <h1 className={`text-center text-9xl font-semibold my-12 w-full`}>
+          Enter your video link
+        </h1>
         <div className="flex justify-center">
-        <input onChange={(e)=>setVid(e.target.value)}className=" text-gray-900 bg-green-400 focus:border-black text-sm rounded-lg  block w-full max-w-5xl h-12" type="text" ></input>
-        <button onClick={(e)=>handleSubmit(e)} className=" mx-5 rounded-lg bg-green-700">CLICK ME!</button>
+          <input
+            onChange={(e) => setVid(e.target.value)}
+            className=" text-gray-900 bg-green-400 focus:border-black text-sm rounded-lg  block w-full max-w-5xl h-12"
+            type="text"
+          ></input>
+          <button
+            onClick={(e) => handleSubmit(e)}
+            className=" mx-5 rounded-lg bg-green-700"
+          >
+            CLICK ME!
+          </button>
         </div>
       </div>
       <div className="flex-col w-3/5 my-12 mx-auto ">
-        <h1 className={`text-center text-5xl font-semibold my-12 w-full`}>Steps to download your video</h1>
+        <h1 className={`text-center text-5xl font-semibold my-12 w-full`}>
+          Steps to download your video
+        </h1>
       </div>
-      
     </main>
-  )
+  );
 }
